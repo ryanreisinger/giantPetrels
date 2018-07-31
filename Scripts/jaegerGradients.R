3242329bafeae023e9ba536d9c8841424bad20ab
Ryan Reisinger <ryan.r.reisinger@gmail.com>
2018-07-31 15:11:45 +0200

    Add script calculating locations based on Jaeger et al. 2010 gradients.

---
+++ Scripts/jaegerGradients.R
@@ -0,0 +1,11 @@
+# Calculate expected d15N based on latitude v. d15N regression presented in Jaeger et al. 2010
+
+# NGPs
+x <- -44.12 #mean lat
+y <- (0.20*x) + 23.03 #gives expected d15N
+y #expected d15N value
+
+# SGPs
+x <- -50.18 #mean lat
+y <- (0.20*x) + 23.03 #gives expected d15N
+y #expected d15N value
